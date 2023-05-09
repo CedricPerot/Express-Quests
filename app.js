218c9f3fc45e02396405efd1bb026d27ffbbbcd4
CedricPerot <cedric.perot@gmail.com>
2023-05-09 13:56:54 +0200

    ajout des routes movies et users

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
+
+const movieHandlers = require("./movieHandlers");
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favourite lists");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
