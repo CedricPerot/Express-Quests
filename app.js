776a00e3b16ccec28c6642e8a3141f2c0de1776b
CedricPerot <cedric.perot@gmail.com>
2023-05-10 09:53:25 +0200

    challenge quete express 03

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 
+
 const port = process.env.APP_PORT ?? 5000;
 
 const movieHandlers = require("./movieHandlers");
@@ -12,6 +13,7 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite lists");
 };
 
+app.use(express.json());
 app.get("/", welcome);
 
 app.get("/api/movies", movieHandlers.getMovies);
@@ -27,3 +29,6 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", movieHandlers.postUser); //challenge quete express 03
